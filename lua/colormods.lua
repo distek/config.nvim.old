4a9c1cea7945b4f5e748d31e85d24bc33ef772a6
distek <distek@protonmail.com>
2022-01-02 08:28:56 -0800

    fix colors; gotta fix init on clean machine

--- lua/colormods.lua
+++ lua/colormods.lua
@@ -1,14 +1,14 @@
--- local walPath = vim.fn.expand("~/.cache/wal")
--- package.path = walPath.."/?.lua;" .. package.path
+local walPath = vim.fn.expand("~/.cache/wal")
+package.path = walPath.."/?.lua;" .. package.path
 
--- local walColors = require("colors")
+local walColors = require("colors")
 
--- local wallyImport = require("wally")
--- local darken = require('wally.util').darken
--- local lighten = require('wally.util').lighten
+local wallyImport = require("wally")
+local darken = require('wally.util').darken
+local lighten = require('wally.util').lighten
 
--- vim.cmd("autocmd UIEnter * highlight! VertSplit guifg=" .. walColors.foreground)
--- vim.cmd("autocmd UIEnter * highlight! CursorLine guibg=" .. darken(wallyImport.colors.bg_highlight, 0.3))
--- vim.cmd("autocmd UIEnter * highlight! Visual guibg=" .. wallyImport.colors.bg_highlight)
+vim.cmd("autocmd UIEnter * highlight! VertSplit guifg=" .. walColors.foreground)
+vim.cmd("autocmd UIEnter * highlight! CursorLine guibg=" .. darken(wallyImport.colors.bg_highlight, 0.3))
+vim.cmd("autocmd UIEnter * highlight! Visual guibg=" .. wallyImport.colors.bg_highlight)
 
--- vim.cmd("highlight! NvimTreeNormal guibg=" .. darken(wallyImport.colors.bg_highlight, 0.5))
+vim.cmd("highlight! NvimTreeNormal guibg=" .. darken(wallyImport.colors.bg_highlight, 0.5))
